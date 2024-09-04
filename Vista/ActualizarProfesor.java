0afd12fee1e7540abf5fc693271e5d5560aa27d5
JohanBltr11 <sebas08beltran@gmail.com>
2024-09-03 20:23:08 -0500

    Update ActualizarProfesor.java

--- Vista/ActualizarProfesor.java
+++ Vista/ActualizarProfesor.java
@@ -23,18 +23,6 @@ public class ActualizarProfesor extends JFrame{
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ActualizarProfesor window = new ActualizarProfesor();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
