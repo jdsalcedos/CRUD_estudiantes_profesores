11cd2a826e4db356bf82479cdfb23954c640f38e
JohanBltr11 <sebas08beltran@gmail.com>
2024-09-03 20:24:13 -0500

    Update ListarProfesor.java

--- Vista/ListarProfesor.java
+++ Vista/ListarProfesor.java
@@ -17,18 +17,6 @@ public class ListarProfesor extends JFrame{
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ListarProfesor window = new ListarProfesor();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
