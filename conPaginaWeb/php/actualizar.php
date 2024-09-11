bf9ded4f08a4f95631e4eae894ae90e3926eb27a
jdsalcedos <juandavidsalcedo85@gmail.com>
2024-09-11 11:44:34 -0500

    version definitiva que funciona

--- conPaginaWeb/php/actualizar.php
+++ conPaginaWeb/php/actualizar.php
@@ -17,6 +17,8 @@ $row=mysqli_fetch_array($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <title>Actualizar usuarios</title>
 </head>
 <body>
